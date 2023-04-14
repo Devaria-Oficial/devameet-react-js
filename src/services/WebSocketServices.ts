2503a00a4d0e1a43260a2935f60d640e0e124589
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2023-04-13 22:38:06 -0300

    Revert "Aula 09 parte 02"
    This reverts commit c9ffecadbecd21bd439c13ff1bc5e3a188f2331e.

--- src/services/WebSocketServices.ts
+++ src/services/WebSocketServices.ts
@@ -1,18 +1,10 @@
 import { io } from 'socket.io-client'
 
-function capitalizeFirstLetter(string: string):string{
-    return string.charAt(0).toUpperCase()+string.slice(1);
-}
-
 class PeerConnectionSession {
     
     _room: any;
     _userId: any;
     socket: any;
-    peerConnections = {} as any;
-    senders = [] as any;
-    listeners = [] as any;
-    ons = [] as any;
 
     constructor(socket : any){
         this.socket = socket;
@@ -42,53 +34,6 @@ class PeerConnectionSession {
 
     updateUserMute(data: any){
         this.socket.emit('toggl-mute-user', data);
-    }
-
-    addPeerConnection(id: string, stream: any, callback: any){
-        if(!this.peerConnections[id]){
-            this.peerConnections[id] = new window.RTCPeerConnection({
-                iceServers: [{urls: 'stun:stun.l.google.com:19302'}]
-            });
-
-            stream?.getTracks().forEach((track: any) => {
-                this.senders.push(this.peerConnections[id].addTrack(track, stream));
-            });
-
-            this.listeners[id] = (event: any) => {
-                const on = '_on' + capitalizeFirstLetter(this.peerConnections[id].connectionState);
-                const fn = this.ons[on];
-                fn && fn(event, id);
-            }
-
-            this.peerConnections[id].addEventListener('connectionstatechange', this.listeners[id]);
-            this.peerConnections[id].ontrack = ({stream: [stream]} : any) => {
-                callback(stream);
-            }
-        }
-    }
-
-    removePeerConnection(id:string){
-        if(this.peerConnections[id]){
-            this.peerConnections[id].removeEventListener('connectionstatechange', this.listeners[id]);
-            delete this.peerConnections[id];
-            delete this.listeners[id];
-        }
-    }
-
-    onAddUser(callback: any){
-        this.socket.on(`${this._room}-add-user`, ({user}: any) => {
-            console.log('onAddUser', user);
-            callback(user);
-        });
-    }
-
-    async callUser(to:any){
-        console.log('callUser to:', to, this.peerConnections[to]?.iceConnectionState);
-        if(this.peerConnections[to]?.iceConnectionState === 'new'){
-            const offer = await this.peerConnections[to].createOffer();
-            await this.peerConnections[to].setLocalDescription(new RTCSessionDescription(offer));
-            this.socket.emit('call-user', {offer, to, link: this._room});
-        }
     }
 }
 
