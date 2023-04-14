381f7d0194635938d96afc2d844a2d8d8e8db453
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2023-04-13 23:45:06 -0300

    aula 09 parte 02 parcial

--- src/services/WebSocketServices.ts
+++ src/services/WebSocketServices.ts
@@ -1,10 +1,18 @@
 import { io } from 'socket.io-client'
 
+function capitalizeFirstLetter(string:string): string {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 class PeerConnectionSession {
     
     _room: any;
     _userId: any;
     socket: any;
+    peerConnections = {} as any;
+    senders = [] as any;
+    listener = [] as any;
+    ons = [] as any;
 
     constructor(socket : any){
         this.socket = socket;
@@ -34,6 +42,79 @@ class PeerConnectionSession {
 
     updateUserMute(data: any){
         this.socket.emit('toggl-mute-user', data);
+    }
+
+    addPeerConnection(id: string, stream: any, callback: any){
+        if(!this.peerConnections[id]){
+            
+            this.peerConnections[id] = new window.RTCPeerConnection({
+                iceServers : [{urls: 'stun:stun.l.google.com:19302'}]
+            });
+            console.log('criou o peer,', id);
+
+            stream.getTracks().forEach((track: any) => {
+                this.senders.push(this.peerConnections[id].addTrack(track, stream));
+            });
+            console.log('adicionou o track');
+
+            this.listener[id] = (event: any) => {
+                const on = '_on' + capitalizeFirstLetter(this.peerConnections[id].connectionState);
+                const fn = this.ons[on];
+                fn && fn(event, id);
+            }
+            console.log('adicionou o listener', id);
+
+            this.peerConnections[id].addEventListener('connectionstatechange', this.listener[id]);
+
+            console.log('adicionou o addEventListener', id);
+
+            this.peerConnections[id].ontrack = ({streams: [stream]}: any) => {
+                console.log('entrou na callback', id);
+                callback(stream);
+            }
+        }
+    }
+
+    removePeerConnection(id:string){
+        if(this.peerConnections[id]){
+            this.peerConnections[id].removeEventListener('connectionstatechange', this.listener[id]);
+            delete this.peerConnections[id];
+            delete this.listener[id];
+        }
+    }
+
+    onAddUser(callback: any){
+        this.socket.on(`${this._room}-add-user`, ({user} : any) => {
+            callback(user);
+        });
+    }
+
+    async callUser(to: any){
+        console.log('callUser to:', to, this.peerConnections[to].iceConnectionState);
+        if(this.peerConnections[to].iceConnectionState === 'new'){
+            const offer = await this.peerConnections[to].createOffer();
+            await this.peerConnections[to].setLocalDescription(new RTCSessionDescription(offer));
+            this.socket.emit('call-user', {offer, to, link: this._room});
+        }
+    }
+
+    async onCallMade(){
+        this.socket.on('call-made', async(data : any) => {
+            console.log('call-made:', data.socket);
+            console.log('call-made:', this.peerConnections[data.socket].iceConnectionState);
+            
+            const selectedPeer = this.peerConnections[data.socket];
+            if(selectedPeer){
+                await selectedPeer.setLocalDescription(new RTCSessionDescription(data.offer));
+                //const answer = await selectedPeer.createAnswer();
+                //await selectedPeer.setLocalDescription(new RTCSessionDescription(answer));
+                // this.socket.emit('make-answerr', {
+                //     answer, 
+                //     to: data.socket, 
+                //     link: this._room
+                // });
+            }
+        });
     }
 }
 
